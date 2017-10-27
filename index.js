0a43841a351eb8b0d52be8f8f6a1f0b6ac5bbcf1
kojitaniguchi <tanigu0511@gmail.com>
2017-10-28 01:23:31 +0900

    set connect

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http,
+// {
+//   path: '/test',
+//   serveClient: false,
+//   pingInterval: 10000,
+//   pingTimeout: 5000,
+//   cookie: false
+// }
+);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  //connected user
+  io.emit('chat message', 'a user connected');
+  //disconnect user
+  socket.on('disconnect', function(){
+    io.emit('chat message', 'user disconnected');
+  });
+  // receive and broadcast message to connected user
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
